c73aec7c35522b93cb734fb4b3bec5b5acb0f8a1
Nguyen Kim Thach <kimthach346@gmail.com>
2022-05-13 02:26:04 +0700

    asm1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,4 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import './App.css';
-class App extends Component {
-
-  render() {
-    return (
-      <div className="App">
-        <p>Sample cho toàn bộ lab và project trong môn học Lập Trình Web Front-End với React với đầy đủ dependency.</p>
-        <p>Phiên bản React hiện tại trên máy của bạn là: {React.version}</p>
-        <p>Phiên bản React đã được sử dụng để thiết kế môn học: 16.14.0.</p>
-      </div>
-    );
-  }
-}
-
-export default App;
-=======
 import { Navbar, NavbarBrand } from 'reactstrap';
 import StaffList from './components/StaffListComponent';
 import './App.css';
@@ -46,4 +29,3 @@ export default App;
 
 
 
->>>>>>> 87349c82e66719664cdead28d2ad2acdbe6e347f
