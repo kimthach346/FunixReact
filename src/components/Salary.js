15494307a23786263b4ca98a45ed55fc6d8b7639
Nguyen Kim Thach <kimthach346@gmail.com>
2022-06-07 20:21:32 +0700

    asm2

--- src/components/Salary.js
+++ src/components/Salary.js
@@ -1,30 +1,39 @@
-import React from "react"
-import { Card, Breadcrumb, BreadcrumbItem, CardFooter } from "reactstrap"
+import React, { useState } from "react"
+import { Card, Breadcrumb, BreadcrumbItem, CardFooter, Button } from "reactstrap"
 import { Link } from "react-router-dom"
 
 function Salary(props) {
-    const salary = props.salaries.map((el) => {
-        return (
-            <div key={el.id} className="col-12 col-md-6 col-lg-4 p-2">
-                <Card className="p-3">
-                    <h3>{el.name}</h3>    
-                    <p>Mã nhân viên: {el.id}</p>
-                    <p>Hệ số lương: {el.salaryScale}</p>
-                    <p>Số ngày làm thêm: {el.overTime}</p>
-                    <CardFooter>
-                        Lương: {(el.salaryScale*3000000 + el.overTime*200000).toFixed(0)}
-                    </CardFooter>
-                </Card>
-            </div>
-        )
-    })
+    
+    const [descSort, setDesc] = useState(false)
+    const salary = props.salaries
+        // .filter((el) => )
+        .map((el) => {
+            return (
+                <div className="col-12 col-md-6 col-lg-4 p-2">
+                    <Card className="p-3">
+                        <h3>{el.name}</h3>    
+                        <p>Mã nhân viên: {el.id}</p>
+                        <p>Hệ số lương: {el.salaryScale}</p>
+                        <p>Số ngày làm thêm: {el.overTime}</p>
+                        <CardFooter>
+                            Lương: {(el.salaryScale*3000000 + el.overTime*200000).toFixed(0)}
+                        </CardFooter>
+                    </Card>
+                </div>
+            )
+        })
 
+    function sortSalary() {
+
+    }
+    
     return (
         <div className="container">
             <Breadcrumb>
                 <BreadcrumbItem><Link to="/staffList">Nhân Viên</Link></BreadcrumbItem>
                 <BreadcrumbItem active>Bảng Lương</BreadcrumbItem>
             </Breadcrumb>
+            <Button onClick={sortSalary()}>Sắp xếp theo lương</Button>
             <div className="row">
                 {salary}
             </div>
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,32 +1,52 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 
 function StaffList(props) {
-
-    const staffList = props.staffs.map((staff) => {
-        return (
-            <div key={staff.id} className="col-6 col-md-4 col-lg-2 p-2">
-                <Link to={`/staffList/${staff.id}`}>
-                    <div>
-                        <img width="100%" src={staff.image} alt ={staff.name}></img>
-                        <h5 className="text-center">{staff.name}</h5>
-                    </div>
-                </Link>
-            </div>
-        )
-    })
-
+    
+    const [searchTerm, findStaff] = useState("")
+    const staffList = props.staffs
+               
+        .map((staff) => {
+            return (
+                <div key={staff.id} className="col-6 col-md-4 col-lg-2 p-2">
+                    <Link to={`/staffList/${staff.id}`}>
+                        <div>
+                            <img width="100%" src={staff.image} alt ={staff.name}></img>
+                            <h5 className="text-center">{staff.name}</h5>
+                        </div>
+                    </Link>
+                </div>
+            )
+        })
+    
     return (
         <div className="container">
             <h3>Nhân Viên</h3>
-            
+            <input type="text" value={searchTerm} 
+                                placeholder="Enter name..." 
+                                onChange={(e) => findStaff(e.target.value)} />
+            {/* <select className="custom-select my-2"
+                    value={selected}
+                   onChange={(e) => {
+
+                       findDept(e.target.value)
+                    }}>
+                <option value="">Phòng Ban</option>
+                <option value="Sale">Sale</option>
+                <option value="HR">HR</option>
+                <option value="IT">IT</option>
+                <option value="Marketing">Marketing</option>
+                <option value="Finance">Finance</option>
+            </select> */}
             <hr />
             <div className="row">
                 {staffList}
             </div>
         </div>
     )
-
+    
 }
+
+
 
 export default StaffList
